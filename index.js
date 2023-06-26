a87597e52b8424ba141012c74b597d6bdbab4d2c
Ayyoob Ajward <ayyoob01cool@gmail.com>
2023-06-26 10:23:50 +0530

    JS

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("i like pizza!");
+console.log("Its really good");
+
+// window.alert("I really love pizza!");
+
+let myName = "Hello";
+let age = 22;
+age += 1;
+console.log(myName + age);
+
+
+document.getElementById("p1").innerHTML = "Hello " + myName;
